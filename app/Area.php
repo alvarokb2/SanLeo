e03fd22b75c10ab0f6f51633a2daf3b50ab5dd39
belaly <gladys.bta@gmail.com>
2017-05-29 16:04:16 -0400

    relaciones de modelos

--- app/Area.php
+++ app/Area.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Area extends Model
 {
-    //
+    protected $table = 'areas';
+    protected $fillable = ['id_informe','name'];
+    protected $guarded = ['id'];
+
+
+    public function informe()
+    {
+        return $this->belongsTo('Sanleo\Informe');
+    }
+
+
 }
--- resources/views/educadora/index.blade.php
+++ resources/views/educadora/index.blade.php
@@ -5,3 +5,10 @@
  * Date: 29-05-17
  * Time: 15:17
  */
+@extends('layouts.app')
+
+@section('content')
+    @include('admin.partials.menu')
+    <br>
+    @yield('contenido')
+@endsection
